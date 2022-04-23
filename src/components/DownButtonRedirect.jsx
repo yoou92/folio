9b8e0af7b5a0ca951c3e94ccd9947a41ea5320ab
Bouzidi Wassini <wassini@ublo.immo>
2022-04-23 20:08:30 +0200

    [ADD] (src/components): DownButtonRedirect

---
+++ src/components/DownButtonRedirect.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-scroll";
+import { HiArrowNarrowDown } from "react-icons/hi";
+
+const DownButtonRedirect = (props) => {
+  return (
+    <div class="invisible sm:visible">
+      <Link to={props.to} smooth={true} duration={350}>
+        <button className="group border-2 px-6 py-3 my-2 flex items-center hover:bg-secondary hover:border-secondary rounded-xl">
+          <HiArrowNarrowDown />
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default DownButtonRedirect;
