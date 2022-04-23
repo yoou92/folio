4e48458f53ccfe18fbc5d03d9552d6364f2301bb
Bouzidi Wassini <wassini@ublo.immo>
2022-04-23 19:25:17 +0200

    [MOD] (Home.jsx): update texts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,45 @@
 import React from "react";
+import { Link } from "react-scroll";
 import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
   return (
     <div name="home" className="w-full h-screen">
-      {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-pink-600">Hi, my name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-          Clint Briley
-        </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          I'm a Full Stack Developer.
-        </h2>
-        <p className="text-[#8892b0] py-4 max-w-[700px]">
-          I’m a full-stack developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
-          building responsive full-stack web applications.
-        </p>
-        <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
-            View Work
-            <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="ml-3 " />
-            </span>
-          </button>
+        <div className="text-secondary font-bold text-xl">Salutations !</div>
+        <div className="text-4xl sm:text-7xl font-bold text-white">
+          Wassini Bouzidi
+        </div>
+        <div className="text-4xl sm:text-6xl font-bold text-gray-400">
+          Développeur chez Epitech Paris
+        </div>
+        <div className="text-white py-4 max-w-[700px] mt-6">
+          <p class="inline">
+            Passionné par le développement de jeux vidéo, de système automatisé
+          </p>{" "}
+          <p class="inline underline underline-offset-2">(C / C++)</p> mais
+          également du développement web{" "}
+          <p class="inline underline underline-offset-2">
+            (JS, TS, React, Node, TailwindCSS)
+          </p>
+          .
+          <div class="mt-2">
+            À la recherche d'un part-time de deux jours par semaine d'octobre à
+            avril, suivi d'un stage d'avril à août afin d'appronfondir mes
+            connaissances, d'évoluer dans un domaine qui me plait et de pourvoir
+            satisfaire un besoin.
+          </div>
+          <div class="mt-10 font-semibold">Prêt à me découvrir ?</div>
+        </div>
+        <div class="text-white flex items-center">
+          <Link to="about" smooth={true} duration={350}>
+            <button className="group border-2 px-6 py-3 my-2 flex items-center hover:bg-secondary hover:border-secondary rounded-xl">
+              C'est parti !
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-3 " />
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
