cebe2be19be83d4b7d2a8344b8554a10f950b1ab
Bouzidi Wassini <wassini@ublo.immo>
2022-04-23 20:08:23 +0200

    [ADD] (src/components): UpButtonRedirect

---
+++ src/components/UpButtonRedirect.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-scroll";
+import { HiArrowNarrowUp } from "react-icons/hi";
+
+const UpButtonRedirect = (props) => {
+  return (
+    <div class="flex justify-end invisible sm:visible">
+      <Link to={props.to} smooth={true} duration={350}>
+        <button className="group border-2 px-6 py-3 my-2 flex items-center hover:bg-secondary hover:border-secondary rounded-xl">
+          <HiArrowNarrowUp />
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default UpButtonRedirect;
