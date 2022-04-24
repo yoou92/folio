3c483e7b05e0f62c2a9fc1e17045fc4616bc76ca
Bouzidi Wassini <wassini@ublo.immo>
2022-04-24 16:48:51 +0200

    [ADD] (Work.jsx): Projet component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,27 @@
 import React from "react";
-import WorkImg from "../assets/workImg.jpeg";
-import realEstate from "../assets/realestate.jpg";
+import ublo from "../assets/ublo.png";
+import arcade from "../assets/arcade.png";
+import malloc from "../assets/malloc.png";
+import shell from "../assets/42sh.png";
+import rpg from "../assets/rpg.png";
+import web from "../assets/web.jpg";
+
+const Projet = (props) => {
+  return (
+    <div>
+      <div class="w-72 text-center font-bold text-2xl">{props.name}</div>
+      <div class="w-72 h-60 rounded-2xl text-white overflow-hidden cursor-pointer transition-all duration-700 hover:rotate-180">
+        <div class=" inset-0 w-full h-full flex justify-center items-center bg-white transition-all duration-500 delay-200 z-20 hover:opacity-0 ">
+          <img class={props.fullScreen ? "h-full" : ""} src={props.logo} />
+        </div>
+        <div class="absolute inset-0 w-full h-full flex justify-center items-center bg-black transition-all z-10 rotate-180">
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Work = () => {
   return (
@@ -8,154 +29,17 @@ const Work = () => {
       <div class="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div class="pb-8">
           <p class="text-4xl font-bold inline border-b-4 text-gray-300 border-secondary">
-            Work
+            Expériences
           </p>
-          <p class="py-6">// Check out some of my recent work</p>
         </div>
-
         {/* Container */}
         <div class="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
-            class="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div class="opacity-0 group-hover:opacity-100">
-              <span class="text-2xl font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-              <div class="pt-8 text-center">
-                <a href="/">
-                  <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${realEstate})` }}
-            class="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div class="opacity-0 group-hover:opacity-100">
-              <span class="text-2xl font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-              <div class="pt-8 text-center">
-                <a href="/">
-                  <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
-            class="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div class="opacity-0 group-hover:opacity-100">
-              <span class="text-2xl font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-              <div class="pt-8 text-center">
-                <a href="/">
-                  <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${realEstate})` }}
-            class="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div class="opacity-0 group-hover:opacity-100">
-              <span class="text-2xl font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-              <div class="pt-8 text-center">
-                <a href="/">
-                  <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
-            class="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div class="opacity-0 group-hover:opacity-100">
-              <span class="text-2xl font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-              <div class="pt-8 text-center">
-                <a href="/">
-                  <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${realEstate})` }}
-            class="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div class="opacity-0 group-hover:opacity-100">
-              <span class="text-2xl font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-              <div class="pt-8 text-center">
-                <a href="/">
-                  <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
+          <Projet name="Ublo" logo={ublo} fullScreen={false} />
+          <Projet name="Arcade" logo={arcade} fullScreen={true} />
+          <Projet name="my_malloc" logo={malloc} fullScreen={true} />
+          <Projet name="42sh" logo={shell} fullScreen={true} />
+          <Projet name="my_rpg" logo={rpg} fullScreen={true} />
+          <Projet name="my_web" logo={web} fullScreen={true} />
         </div>
       </div>
     </div>
