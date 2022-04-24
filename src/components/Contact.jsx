c93257887c1d074d2e057b22e41d66657b6d80fe
Bouzidi Wassini <wassini@ublo.immo>
2022-04-24 20:21:54 +0200

    [ADD] (Contact.jsx): button up and down + text field form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import DownButtonRedirect from "./DownButtonRedirect";
+import UpButtonRedirect from "./UpButtonRedirect";
 
 const Contact = () => {
   return (
@@ -9,37 +11,38 @@ const Contact = () => {
       <form
         method="POST"
         action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c"
-        class="flex flex-col max-w-[600px] w-full"
+        class="flex flex-col max-w-[600px] w-full text-white"
       >
         <div class="pb-8">
-          <p class="text-4xl font-bold inline border-b-4 border-secondary text-gray-300">
+          <p class="text-4xl font-bold inline border-b-4 border-secondary text-white">
             Contact
-          </p>
-          <p class="text-gray-300 py-4">
-            // Submit the form below or shoot me an email - myemail@gmail.com
           </p>
         </div>
         <input
-          class="bg-[#ccd6f6] p-2"
+          class="rounded-md bg-secondary text-white placeholder-gray-400 p-2"
           type="text"
           placeholder="Name"
           name="name"
         />
         <input
-          class="my-4 p-2 bg-[#ccd6f6]"
+          class="rounded-md my-4 p-2 bg-secondary text-white placeholder-gray-400"
           type="email"
           placeholder="Email"
           name="email"
         />
         <textarea
-          class="bg-[#ccd6f6] p-2"
+          class="rounded-md bg-secondary text-white placeholder-gray-400 p-2"
           name="message"
           rows="10"
           placeholder="Message"
         ></textarea>
-        <button class="text-white border-2 hover:bg-secondary hover:border-secondary px-4 py-3 my-8 mx-auto flex items-center">
-          Let's Collaborate
+        <button class="text-white border-2 hover:bg-secondary hover:border-secondary px-4 py-3 my-8 mx-auto flex items-center rounded-xl">
+          Collaborons !
         </button>
+        <div class="flex justify-between">
+          <DownButtonRedirect to="home" />
+          <UpButtonRedirect to="work" />
+        </div>
       </form>
     </div>
   );
