0cf550f5612080e6b414cdc3b7dff3f78c38dc60
Bouzidi Wassini <wassini@ublo.immo>
2022-04-26 17:53:15 +0200

    [MOD] (Work.jsx): overflow projet

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -7,19 +7,105 @@ import rpg from "../assets/rpg.png";
 import web from "../assets/web.jpg";
 import DownButtonRedirect from "./DownButtonRedirect";
 import UpButtonRedirect from "./UpButtonRedirect";
+import "./ScrollBar.css";
 
 const Projet = (props) => {
   return (
-    <div>
-      <div class="w-72 text-center font-bold text-2xl">{props.name}</div>
-      <div class="w-72 h-60 rounded-2xl text-white overflow-hidden cursor-pointer transition-all duration-700 hover:rotate-180">
-        <div class="sticky inset-0 w-full h-full flex justify-center items-center bg-white transition-all duration-500 delay-200 z-20 hover:opacity-0 ">
-          <img class={props.fullScreen ? "h-full" : ""} src={props.logo} />
-        </div>
-        <div class="sticky inset-0 w-full h-full flex justify-center items-center bg-black transition-all z-10 rotate-180">
-          {props.name}
-        </div>
+    <div class="rounded-xl">
+      <div class="flex items-center justify-center w-full h-full bg-white rounded-xl object-center">
+        <img
+          class="rounded-xl bg-cover bg-center"
+          src={props.logo}
+          alt={props.name}
+        />
       </div>
+      <div class="px-6 py-4">
+        <div class="text-white font-bold text-xl mb-8">{props.name}</div>
+        {props.description.map((text, i) => (
+          <div class="text-white text-base">{text}</div>
+        ))}
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        {props.techUsed.map((tech, i) => (
+          <span class="inline-block bg-white rounded-full px-3 py-1 text-sm font-semibold text-secondary mr-2 mb-2">
+            {tech}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Projets = () => {
+  return (
+    <div class="overflow-y-auto h-4/6 shadow-lg shadow-main space-y-36 border-main border-2 rounded p-4">
+      <Projet
+        name="Ublo"
+        logo={ublo}
+        description={[
+          "Stage de 6 mois en tant que développer Full-Stack chez Ublo.immo.",
+          "Ublo vise à simplifier la gestion locative par l'intermédiaire d'un logiciel de gestion immobilière intuitif et efficace",
+          "J'ai été chargé de développer dans un premier temps un formulaire d'état des lieux d'entrée et de sortie devant être rempli sur une tablette",
+          "Et dans second temps, de travailler sur le site web d'Ublo pour les clients",
+        ]}
+        techUsed={[
+          "TypeScript",
+          "JavaScript",
+          "React",
+          "NodeJS",
+          "React-Admin",
+          "TailwindCSS",
+          "Styled-Components",
+          "Postman",
+          "Docker",
+          "PostgreSQL",
+          "MongoDB",
+          "GitHub",
+        ]}
+      />
+      <Projet
+        name="Arcade"
+        logo={arcade}
+        description={["tres intéreasn"]}
+        techUsed={["C++", "GitHub"]}
+      />
+      <Projet
+        name="my_malloc"
+        logo={malloc}
+        description={["tres intéreasn"]}
+        techUsed={["C", "GitHub"]}
+      />
+      <Projet
+        name="42sh"
+        logo={shell}
+        description={["tres intéreasn"]}
+        techUsed={["C", "GitHub"]}
+      />
+      <Projet
+        name="my_rpg"
+        logo={rpg}
+        description={["tres intéreasn"]}
+        techUsed={["C", "GitHub"]}
+      />
+      <Projet
+        name="my_web"
+        logo={web}
+        description={["tres intéreasn"]}
+        techUsed={[
+          "TypeScript",
+          "JavaScript",
+          "React",
+          "NodeJS",
+          "React-Admin",
+          "TailwindCSS",
+          "Styled-Components",
+          "Postman",
+          "Docker",
+          "PostgreSQL",
+          "MongoDB",
+          " GitHub",
+        ]}
+      />
     </div>
   );
 };
@@ -29,20 +115,12 @@ const Work = () => {
     <div name="work" class="w-full md:h-screen text-gray-300 ">
       <div class="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div class="pb-8">
-          <p class="text-4xl font-bold inline border-b-4 text-gray-300 border-secondary">
+          <p class="text-4xl font-bold inline border-b-4 text-white border-secondary">
             Expériences
           </p>
         </div>
-        {/* Container */}
-        <div class="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          <Projet name="Ublo" logo={ublo} fullScreen={false} />
-          <Projet name="Arcade" logo={arcade} fullScreen={true} />
-          <Projet name="my_malloc" logo={malloc} fullScreen={true} />
-          <Projet name="42sh" logo={shell} fullScreen={true} />
-          <Projet name="my_rpg" logo={rpg} fullScreen={true} />
-          <Projet name="my_web" logo={web} fullScreen={true} />
-        </div>
-        <div class="flex py-6 justify-between">
+        <Projets />
+        <div class="flex p-4 justify-between">
           <DownButtonRedirect to="contact" />
           <UpButtonRedirect to="skills" />
         </div>
