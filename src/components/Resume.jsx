1c088fb3d194c5b775242e121ed4eb1b3f3fa1d7
Bouzidi Wassini <wassini@ublo.immo>
2022-04-29 21:05:25 +0200

    [ADD] (Resume.jsx): DownloadResume component + override default download name

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,23 +1,30 @@
-import React from "react"
+import React from "react";
 import resumePng from "../assets/resume/resume.png";
 
+const DownloadResume = (props) => {
+  return (
+    <div class="inline-flex space-x-10 p-10">
+      <a
+        href={props.file}
+        class="text-2xl p-4 font-semibold text-white rounded-xl transition-all duration-500 bg-gradient-to-br to-secondary via-black from-main bg-size-200 hover:bg-right-bottom"
+        download="wassini_bouzidi_cv"
+      >
+        Télécharger en {props.ext}
+      </a>
+    </div>
+  );
+};
+
 const Resume = () => {
+  return (
+    <div class="text-center bg-gradient-to-br from-main to-secondary">
+      <div class="w-full h-full flex justify-center p-4">
+        <img class="rounded-xl" src={resumePng} alt="resume" />
+      </div>
+      <DownloadResume ext="PDF" file={resumePng} />
+      <DownloadResume ext="PNG" file={resumePng} />
+    </div>
+  );
+};
 
-    return (
-        <div class="text-center bg-gradient-to-br from-main to-secondary">
-            <div class="w-full h-full flex justify-center p-4">
-                <img class="rounded-xl" src={resumePng} alt="resume" />
-            </div>
-            <div class="space-x-10 p-10">
-                <a href={resumePng} class="text-2xl p-4 font-semibold text-white bg-main rounded-xl hover:bg-secondary transition-all duration-500">
-                    Télécharger en PDF
-                </a>
-                <a href={resumePng} class="text-2xl p-4 font-semibold text-white bg-main rounded-xl hover:bg-secondary transition-all duration-500">
-                    Télécharger en PNG
-                </a>
-            </div>
-        </div>
-    );
-}
-
-export default Resume
+export default Resume;
