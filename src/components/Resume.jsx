63515f3c61f6f1151d98debefa55ecfb24020951
Bouzidi Wassini <wassini@ublo.immo>
2022-04-30 23:04:43 +0200

    [FIX] (Resume.jsx): resume background size for phone

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,7 +3,7 @@ import resumePng from "../assets/resume/resume.png";
 
 const DownloadResume = (props) => {
   return (
-    <div class="inline-flex space-x-10 p-10">
+    <div class="inline-flex space-x-10 p-10 hover:scale-125">
       <a
         href={props.file}
         class="text-2xl p-4 font-semibold text-white rounded-xl transition-all duration-500 bg-gradient-to-br to-secondary via-black from-main bg-size-200 hover:bg-right-bottom"
@@ -17,9 +17,13 @@ const DownloadResume = (props) => {
 
 const Resume = () => {
   return (
-    <div class="text-center bg-gradient-to-br from-main to-secondary">
-      <div class="w-full h-full flex justify-center p-4">
-        <img class="rounded-xl" src={resumePng} alt="resume" />
+    <div class="w-full h-full text-center bg-gradient-to-br from-main to-secondary">
+      <div class="flex justify-center p-4">
+        <img
+          src={resumePng}
+          class="rounded-xl object-cover w-full h-full"
+          alt="resume"
+        />
       </div>
       <DownloadResume ext="PDF" file={resumePng} />
       <DownloadResume ext="PNG" file={resumePng} />
